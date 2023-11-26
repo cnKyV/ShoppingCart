98035035ee411c1647d5f4f4f9fd2d3df5cbc484
cnKyV <31133723+cnKyV@users.noreply.github.com>
2023-11-26 19:14:35 +0300

    custom json parser #2

--- ShoppingCart.Cpp/JsonParser/jsonparser.hpp
+++ ShoppingCart.Cpp/JsonParser/jsonparser.hpp
@@ -5,9 +5,10 @@
 #ifndef SHOPPINGCART_CPP_JSONPARSER_HPP
 #define SHOPPINGCART_CPP_JSONPARSER_HPP
 
-#define OpeningBracketAscii 123
-#define ClosingBracketAscii 125
-#define QuoteAscii 34
+#define OPENING_BRACKET_ASCII 123
+#define CLOSING_BRACKET_ASCII 125
+#define QUOTE_ASCII 34
+#define COLON_ASCII 58
 
 
 #include <iostream>
@@ -25,7 +26,7 @@ public:
     template<typename T>
     T parseJsonToObj(std::string& jsonAsString)
     {
-        std::map<std::string, std::string>* deconstructedKeyValuePairs = new std::map<std::string, std::string>();
+        std::map<jsonData, jsonData>* deconstructedKeyValuePairs = new std::map<jsonData, jsonData>();
 
         if (jsonAsString[0] != '{')
             throw std::runtime_error("The string provided is not json.");
@@ -45,27 +46,58 @@ public:
 private:
     static std::string _input;
 
+    struct jsonData{
+        std::string path;
+        size_t indent;
+        jsonData* parent;
+        std::map<std::string, std::string> keyValuePair;
+    };
+
     template<typename T>
-    void asciiMatcher(char& word, T& obj, std::string input = _input)
+    std::map<jsonData, jsonData>* asciiMatcher(T& obj, std::string input = _input)
     {
         bool _isFinished = false;
         bool _isReadingOpen = false;
+        bool _isInsideQuote = false;
         size_t _indent = 0;
         size_t _index = 0;
 
+
+
         while(!_isFinished)
         {
+            wchar_t word = input[_index];
+
+
+            switch (word) {
+                case OPENING_BRACKET_ASCII:
+                    _isReadingOpen = true;
+                    break;
+                case CLOSING_BRACKET_ASCII:
+                    _isReadingOpen = false;
+                    break;
+                case QUOTE_ASCII:
+                    if (!_isInsideQuote)
+                    {
+                        _isInsideQuote = true;
+                    }
+                    else
+                    {
+                        _isInsideQuote = false;
+                    }
+                    break;
+                default:
 
 
 
-        }
+            }
 
 
-        if (word == OpeningBracketAscii)
-        {
-            _isReadingOpen = true;
             ++_index;
         }
+
+
+
 
 
 
